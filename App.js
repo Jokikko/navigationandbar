669d7d1d37e4d193f42ef3370c39e5d48c255275
Tuomas Jokikokko <t1jotu01@students.oamk.fi>
2023-09-20 12:42:14 +0300

    completed

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import HomeScreen from './HomeScreen';
+import SecondScreen from './SecondScreen';
 
 export default function App() {
+  const stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <stack.Navigator initialRouteName="Home">
+      <stack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        title: 'Home',
+        headerTitle: 'Home',
+      }}
+      />
+      <stack.Screen
+      name="Second"
+      component={SecondScreen}
+      options={{
+        title: 'Second',
+        hedertitle: 'Second'
+      }}
+      />
+    </stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
